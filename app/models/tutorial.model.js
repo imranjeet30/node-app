a2270b3b872deac3fdf9651b6761826abd24a349
Ranjeet Thakur <imranjeet30@gmail.com>
2024-02-13 09:28:00 +0530

    tutorial crud operation added

--- app/models/tutorial.model.js
+++ app/models/tutorial.model.js
@@ -1,27 +1,27 @@
 module.exports = (sequelize, Sequelize) => {
-    const Joi = require('joi');
+	const Joi = require("joi");
 
-    const schema = Joi.object({
-        title:Joi.string().required(),
-        description:Joi.string().required(),
-        publish:Joi.boolean().required(),
-    });
+	const schema = Joi.object({
+		title: Joi.string().required(),
+		description: Joi.string().required(),
+		publish: Joi.boolean().required(),
+	});
 
-    const Tutorial = sequelize.define('tutorial', {
-        title:{
-            type:Sequelize.STRING
-        },
-        description:{
-            type:Sequelize.STRING
-        },
-        publish:{
-            type:Sequelize.BOOLEAN
-        }
-    });
+	const Tutorial = sequelize.define("tutorial", {
+		title: {
+			type: Sequelize.STRING,
+		},
+		description: {
+			type: Sequelize.STRING,
+		},
+		publish: {
+			type: Sequelize.BOOLEAN,
+		},
+	});
 
-    const validate = (data) => {
-        return schema.validate(data);
-    };
+	const validate = (data) => {
+		return schema.validate(data);
+	};
 
-    return {Tutorial, validate};
-}
+	return { Tutorial, validate };
+};
